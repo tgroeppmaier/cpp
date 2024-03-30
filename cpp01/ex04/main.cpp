a134c27db3078c19dbfa18c738aa7740d82ba882
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-03-30 09:37:00 +0100

    ex04 and naming

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cerrno>  // for errno
+#include <cstring>  // for strerror
+
+int main()
+{
+    std::string fpath_in, fpath_out, line, s1, s2;
+    std::fstream file, newfile;
+
+    while (true){
+        std::cout << "Enter filename\n";
+        std::cin >> fpath_in;
+        file.open(fpath_in.c_str(), std::ios::in);
+        if(!file.is_open())
+            std::cout << std::strerror(errno) << "\n";
+        else
+            break;
+    }
+    
+    fpath_out = fpath_in + ".replace";
+    newfile.open(fpath_out.c_str(), std::ios::out);
+    if (!newfile.is_open()) {
+        std::cerr << std::strerror(errno) << "\n";
+        return 1;
+    }
+
+    std::cout << "Enter s1\n";
+    std::cin >> s1;
+    std::cout << "Enter s2\n";
+    std::cin >> s2;
+    std::cout << "replacing all " << s1 << " with " << s2 << std::endl;
+
+    while (std::getline(file, line)) {
+        std::string::size_type pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos) {
+            line = line.substr(0, pos) + s2 + line.substr(pos + s1.length());
+            pos += s2.length();
+        }
+        newfile << line << "\n";
+    }
+
+    file.close();
+    newfile.close();
+    return 0;
+}
+
