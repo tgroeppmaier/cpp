1e0e6ae4b7ca90f2df8af40affa81d9b98e75c1c
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-05-27 13:00:04 +0200

    tests added

--- cpp05/ex00/main.cpp
+++ cpp05/ex00/main.cpp
@@ -5,38 +5,55 @@
 using std::cout;
 
 int main(){
-    // try {
-    //     Bureaucrat cog1("Karl", 200);
-    // }
-    // catch (Bureaucrat::GradeTooHighException& e) {
-    //     std::cerr << "GradeTooHighException: " << e.what() << '\n';
-    // }
-    // catch (Bureaucrat::GradeTooLowException& e) {
-    //     std::cerr << "GradeTooLowException: " << e.what() << '\n';
-    // }
+    try {
+        Bureaucrat cog1("Karl", 200);
+    }
+    catch (Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "GradeTooHighException: " << e.what() << '\n';
+    }
+    catch (Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "GradeTooLowException: " << e.what() << '\n';
+    }
     
-    // try {
-    //     Bureaucrat cog1("Karl", -10);
-    // }
-    // catch (Bureaucrat::GradeTooHighException& e) {
-    //     std::cerr << "GradeTooHighException: " << e.what() << '\n';
-    // }
-    // catch (Bureaucrat::GradeTooLowException& e) {
-    //     std::cerr << "GradeTooLowException: " << e.what() << '\n';
-    // }
+    try {
+        Bureaucrat cog1("Karl", -10);
+    }
+    catch (Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "GradeTooHighException: " << e.what() << '\n';
+    }
+    catch (Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "GradeTooLowException: " << e.what() << '\n';
+    }
 
     Bureaucrat cog1("Karl", 10);
-    
-    // try { cog1.decGrade(141);
-    // }
-    // catch (Bureaucrat::GradeTooHighException& e) {
-    //     std::cerr << "GradeTooHighException: " << e.what() << '\n';
-    // }
-    // catch (Bureaucrat::GradeTooLowException& e) {
-    //     std::cerr << "GradeTooLowException: " << e.what() << '\n';
-    // }
+    cout << "Name: " << cog1.getName() << '\n';
+    cout << "Grade: " << cog1.getGrade() << '\n';
 
-    try { cog1.incGrade(-10);
+    try {
+        cog1.decGrade(141);
+    }
+    catch (Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "GradeTooHighException: " << e.what() << '\n';
+    }
+    catch (Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "GradeTooLowException: " << e.what() << '\n';
+    }
+
+    try {
+        cog1.incGrade(-10);
+    }
+    catch(Bureaucrat::NegativeNumberException& e) {
+        std::cerr << "NegativeNumberException: " << e.what() << '\n';
+    }
+    catch (Bureaucrat::GradeTooHighException& e) {
+        std::cerr << "GradeTooHighException: " << e.what() << '\n';
+    }
+    catch (Bureaucrat::GradeTooLowException& e) {
+        std::cerr << "GradeTooLowException: " << e.what() << '\n';
+    }
+
+    try {
+        cog1.incGrade(1);
     }
     catch(Bureaucrat::NegativeNumberException& e) {
         std::cerr << "NegativeNumberException: " << e.what() << '\n';
