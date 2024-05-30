bb0c526262d297bdceaa65f9a57c04087fbc31fc
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-05-30 08:15:11 +0200

    test stream

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,17 +1,20 @@
 #include "ScalarConverter.hpp"
 #include <stdlib.h>
 
-void ScalarConverter::convert(std::string& str) {
-    int num;
-    
-    try {
-        num = atoi(str.c_str());
-        // Use num...
-    } catch (std::invalid_argument const &e) {
-        // The string does not represent a valid integer
-    } catch (std::out_of_range const &e) {
-        // The integer represented by the string is out of the range of int
-    }
+using std::cout;
 
-    std::cout << num << '\n';
+void ScalarConverter::convert(std::string& str) {
+    int num = atoi(str.c_str());
+    cout << "int: ";
+    if (num == 0) {
+        cout << "no number\n";
+    }
+    if (num == -1) {
+        cout << "out of bounds\n";
+    }
+    else {
+        cout << num << '\n';
+    }
+    
+
 }
--- cpp06/ex00/convert.cpp
+++ cpp06/ex00/convert.cpp
@@ -1,8 +1,46 @@
 #include "ScalarConverter.hpp"
 
-int main() {
-    std::string str;
-    std::cin >> str;
-    ScalarConverter::convert(str);
+using std::string;
+using std::cout;
+
+// int main(int argc, char* argv[]) {
+//     if (argc != 2) {
+//         cout << "Error. Usage: " << argv[0] << " <literal>" << '\n';
+//         return 1;
+//     }
+    
+//     string str = argv[1];
+    
+//     ScalarConverter::convert(str);
+//     return 0;
+// }
+
+#include <iostream>
+#include <sstream> // for std::stringstream
+#include <string>
+
+int main(int argc, char* argv[])
+{
+    if (argc <= 1)
+    {
+        // On some operating systems, argv[0] can end up as an empty string instead of the program's name.
+        // We'll conditionalize our response on whether argv[0] is empty or not.
+        if (argv[0])
+            std::cout << "Usage: " << argv[0] << " <number>" << '\n';
+        else
+            std::cout << "Usage: <program name> <number>" << '\n';
+
+        return 1;
+    }
+
+    std::stringstream convert; // set up a stringstream variable named convert
+    convert << argv[1]; // insert the input from argv[1] into the stringstream
+
+    int myint = 0;
+    if (!(convert >> myint)) // do the conversion
+        myint = 0; // if conversion fails, set myint to a default value
+
+    std::cout << "Got integer: " << myint << '\n';
+
     return 0;
 }
