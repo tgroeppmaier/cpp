03ab8f5235197a6c26b8c0fd990809581b659ab2
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-06-05 19:29:10 +0200

    main update

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -7,12 +7,18 @@ void print(const T& value) {
     std::cout << value << ' ';
 }
 
+void print_int(int num) {
+    std::cout << num << ' ';
+}
+
 int main() {
     int intArray[] = {1, 2, 3, 4, 5};
     unsigned int intLen = sizeof(intArray) / sizeof(intArray[0]);
 
     iter(intArray, intLen, print<int>);
+    std::cout << std::endl;
 
+    iter(intArray, intLen, print_int);
     std::cout << std::endl;
 
     std::string strArray[] = {"Hello", "World", "from", "iter"};
