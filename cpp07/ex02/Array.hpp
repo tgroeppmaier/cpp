f264ce020f93cb22a9ea14e1460e5980825d5ad4
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-06-06 21:08:55 +0200

    implemented tests

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,18 +6,23 @@
 template <typename T>
 class Array {
 private:
+    int m_len;
     T* m_array;
-    unsigned int m_len;
 
 public:
     Array() : m_len(0) {
         m_array = new T[m_len];
     }
-    Array(unsigned int len) : m_len(len) {
+
+    Array(int len) : m_len(len), m_array(NULL) {
+        if (m_len < 0) {
+            throw std::invalid_argument("Length must be non-negative");
+        }
         m_array = new T[m_len];
     }
+
     Array(const Array& other) : m_len(other.m_len), m_array(new T[other.m_len]) {
-        for (unsigned int i = 0; i < m_len; i++) {
+        for (int i = 0; i < m_len; i++) {
             m_array[i] = other.m_array[i];
         }
     }
@@ -30,7 +35,7 @@ public:
     Array& operator=(const Array& other) {
         if (this != &other) {
             T* tempArray = new T[other.m_len];
-            for (unsigned int i = 0; i < other.m_len; i++) {
+            for (int i = 0; i < other.m_len; i++) {
                 tempArray[i] = other.m_array[i];
             }
             delete[] m_array;
@@ -40,22 +45,22 @@ public:
         return *this;
     }
 
-    T& operator[](unsigned int i) {
-        if (i >= m_len) {
+    T& operator[](int i) {
+        if (i < 0 || i >= m_len) {
             throw std::out_of_range("Index out of range");
         }
         return m_array[i];
     }
 
-    const T& operator[](unsigned int i) const {
-        if (i >= m_len) {
+    const T& operator[](int i) const {
+        if (i < 0 || i >= m_len) {
             throw std::out_of_range("Index out of range");
         }
         return m_array[i];
     }
 
     // member function
-    unsigned int size() const{
+    int size() const{
         return m_len;
     }
 };
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -2,23 +2,48 @@
 #include <cstdlib>
 #include "Array.hpp"
 
+    struct Mystruct {
+        int value;
+    };
 
 #define MAX_VAL 750
 int main(int, char**)
-{
+{   
+    // test complex data type
+    Array<Mystruct> struct_array(10);
+    for (int i = 0; i < 10; i++) {
+        struct_array[i].value = i;
+        std::cout << struct_array[i].value << std::endl;
+    }
+
     Array<int> numbers(MAX_VAL);
+
+    // test size function
+    std::cout << "size = " << numbers.size() << std::endl;
+
+    Array<int> nothing(0);
     int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
+    srand(static_cast<unsigned int>(time(NULL)));
     for (int i = 0; i < MAX_VAL; i++)
     {
         const int value = rand();
         numbers[i] = value;
         mirror[i] = value;
     }
+
     //SCOPE
     {
+        // test copy assignment and deep copy
         Array<int> tmp = numbers;
+        tmp[0] = 777;
+        std::cout << "numbers[0] = " << numbers[0] << std::endl;
+        std::cout << "tmp[0] = " << tmp[0] << std::endl;
+
+        // test copy constructor and deep copy
         Array<int> test(tmp);
+        tmp[0] = 666;
+        std::cout << "test[0] = " << test[0] << std::endl;
+        std::cout << "tmp[0] = " << tmp[0] << std::endl;
     }
 
     for (int i = 0; i < MAX_VAL; i++)
@@ -29,6 +54,8 @@ int main(int, char**)
             return 1;
         }
     }
+
+
     try
     {
         numbers[-2] = 0;
@@ -50,6 +77,7 @@ int main(int, char**)
     {
         numbers[i] = rand();
     }
+
     delete [] mirror;//
     return 0;
 }
