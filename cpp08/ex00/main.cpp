99e7ae91fb20766fd5a0903ea750326688ca44f1
Timo <115063934+tgroeppmaier@users.noreply.github.com>
2024-06-27 13:00:39 +0200

    using assing to populate list

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -11,17 +11,15 @@ int main() {
   std::vector<int> vec(arr, arr + sizeof(arr) / sizeof(arr[0]));
   cout << easyfind(vec, 10) << std::endl;
 
-  // std::list<int> lis(std::begin(arr), std::end(arr));   cpp 11
-  
   std::list<int> lis;
-  std::list<int>::iterator it = lis.begin();
-  for (size_t i = 0; i < vec.size(); ++i) {
-    lis.push_back(vec.at(i));
-    cout << *it << " ";
-    ++it;
-  }
-
+  std::vector<int>::const_iterator start = vec.begin();
+  std::vector<int>::const_iterator end = vec.end();
+  lis.assign(start, end);
+  for (std::list<int>::iterator it = lis.begin(); it != lis.end(); ++it) {
+        std::cout << *it << " ";
+    }
   cout << std::endl;
+
   cout << easyfind(lis, 20) << std::endl;
 
   try {
