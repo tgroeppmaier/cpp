2f8cbbe67d94d9f46a05323b5cd40c53c9db36e6
tgroeppmaier <115063934+tgroeppmaier@users.noreply.github.com>
2024-07-09 18:52:26 +0200

    save

--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,24 +1,96 @@
 #ifndef PMERGE_ME_HPP
 #define PMERGE_ME_HPP
 
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <utility>
 #include <list>
-
+#include <algorithm>
+#include <string>
+#include <deque>
 
 using std::vector;
 using std::list;
+using std::string;
 
 class PmergeMe {
     private:
-
+        template<typename Iterator>
+        static Iterator findInsertPosition(Iterator first, Iterator last, const typename std::iterator_traits<Iterator>::value_type& value) {
+            while (first != last && *first < value) {
+                ++first;
+            }
+            return first;
+        }
 
     public:
-        static void sort_vector(vector<int>& vec);
+        template<typename Container>
+        static void FordJohnsonSort(Container& cont) {
+            if (cont.size() <= 1) 
+                return;
 
-        // static void sort_list(list<int>& lis);
+            // Handle odd-sized container
+        bool hasOdd = false;
+        typename Container::value_type odd;
+        if (cont.size() % 2 != 0) {
+            odd = cont.back();
+            cont.pop_back();
+            hasOdd = true;
+        }
+
+        // Create pairs
+        typedef std::pair<typename Container::value_type, typename Container::value_type> ValuePair;
+        std::vector<ValuePair> pairs;
+        typename Container::iterator it = cont.begin();
+        while (it != cont.end()) {
+            typename Container::value_type first = *it;
+            ++it;
+            typename Container::value_type second = *it;
+            ++it;
+            // Sort the pair internally. Higher number is first
+            if (first > second) {
+                pairs.push_back(std::make_pair(first, second));
+            }
+            else 
+                pairs.push_back(std::make_pair(second, first));
+        }
+
+        // Sort the sequences of pairs by the first value
+        std::sort(pairs.begin(), pairs.end());
+
+        // Initialize result and pendChain
+        Container result, pendChain;
+        
+        // Push the first element of each pair into result, and the second into pendChain
+        for (typename std::vector<ValuePair>::const_iterator pairIt = pairs.begin(); pairIt != pairs.end(); ++pairIt) {
+            result.push_back(pairIt->first);
+            pendChain.push_back(pairIt->second);
+        }
+
+        // Merge pend elements into main chain
+        typename Container::iterator pendIt = pendChain.begin();
+        while (pendIt != pendChain.end()) {
+            result.insert(findInsertPosition(result.begin(), result.end(), *pendIt), *pendIt);
+            ++pendIt;
+        }
+
+        // Insert the odd element if it exists
+        if (hasOdd) {
+            result.insert(findInsertPosition(result.begin(), result.end(), odd), odd);
+        }
+
+        cont = result;
+    }
+
+    template<typename Container>
+    static void printContainer(const Container& cont) {
+        typename Container::const_iterator it;
+        for (it = cont.begin(); it != cont.end(); ++it) {
+            std::cout << *it << " ";
+        }
+        std::cout << std::endl << std::endl;
+    }
 };
 
 
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -1,13 +1,46 @@
 #include "PmergeMe.hpp"
 
-int main() {
-    std::vector<int> vec;
-    int values[] = {55, 6, 3, 1, -7, 0, 2, 4545, -4545};
-    for (size_t i = 0; i < sizeof(values) / sizeof(values[0]); ++i) {
-        vec.push_back(values[i]);
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <number of elements> ..." << std::endl;
+        return 1;
     }
 
-    PmergeMe::sort_vector(vec);
+   std::vector<int> values;
+    char* end;
+    for (int i = 1; i < argc; ++i) {
+        long number = std::strtol(argv[i], &end, 10);
 
+        if (*end != '\0' || number < 1 || number > std::numeric_limits<int>::max()) {
+            std::cout << "Error: Number out of bounds " << argv[i] << std::endl;
+            return 1;
+        }
+        values.push_back(static_cast<int>(number));
+    }
+
+    double duration;
+    std::cout << "Before: ";
+    PmergeMe::printContainer(values);
+
+    clock_t start = clock();
+    PmergeMe::FordJohnsonSort(values);
+    clock_t stop = clock();
+
+    std::cout << "After: ";
+    PmergeMe::printContainer(values);
+    duration = (double)(stop - start) / CLOCKS_PER_SEC * 1000000;
+    std::cout << "Time to process a range of " << values.size() << " elements with std::vector: " 
+        << duration << " us\n";
+
+    std::deque<int> lst;
+    lst.assign(values.begin(), values.end());
+
+    start = clock();
+    PmergeMe::FordJohnsonSort(lst);
+    stop = clock();
+    duration = (double)(stop - start) / CLOCKS_PER_SEC * 1000000;
+    std::cout << "Time to process a range of " << values.size() << " elements with std::deque: " 
+        << duration << " us\n";
+    
     return 0;
 }
